4e1e64267edadf6ed291dcadde0a75159b674ab0
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-05-02 16:28:59 +0700

    Add project files.

---
+++ DBConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAutentikasi
+{
+    public static class DBConfig
+    {
+        public static string dbServer = "localhost";
+        public static string dbUser = "root";
+        public static string dbName = "db_kursus";
+        public static string dbPass = "Dimas210904";
+        public static string dbPort = "3307";
+
+        public static string ConsStr = $"server={dbServer};user={dbUser};database={dbName};password={dbPass};port={dbPort}";
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; 
+
+namespace ProjectAutentikasi
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text.Trim();
+            string password = textBoxPassword.Text.Trim();
+
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Username dan Password harus diisi.", "validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using(MySqlConnection conn = new MySqlConnection(DBConfig.ConsStr))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT password FROM pengguna WHERE username = @username";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        string hashFromDb = reader.GetString("password");
+                        bool isValid = BCrypt.Net.BCrypt.Verify(password, hashFromDb);
+
+                        if (isValid)
+                        {
+                            MessageBox.Show("Login Berhasil!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Password Salah!", "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username Tidak Ditemukan!", "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi Kesalahan koneksi:\n" + ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
